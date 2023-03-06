787a02852a187d6631cca52d78c165b7c39ec20b
tri tin <truongtritin.bee@gmail.com>
2023-03-06 22:44:15 +0700

    complete the daily Ui

--- src/components/DailyInfo.jsx
+++ src/components/DailyInfo.jsx
@@ -1,93 +1,60 @@
 import React from 'react'
 
 function DailyInfo() {
+
+  const dailyInfo = [{
+    id: 1,
+    day: 'Monday',
+    from: "30°",
+    to: "15°"
+  },{
+    id: 2,
+    day: 'Tuesday',
+    from: "25°",
+    to: "15°"
+  },{
+    id: 3,
+    day: 'Thursday',
+    from: "26°",
+    to: "15°"
+  },{
+    id: 4,
+    day: 'Wednesday',
+    from: "27°",
+    to: "15°"
+  },{
+    id: 5,
+    day: 'Friday',
+    from: "28°",
+    to: "15°"
+  },{
+    id: 6,
+    day: 'Saturday',
+    from: "29°",
+    to: "15°"
+  },{
+    id: 7,
+    day: 'Sunday',
+    from: "33°",
+    to: "15°"
+    },]
+  
   return (
     <div>
-      <div className="right-0 top-20 absolute  w-4/6  bg-white mt-3 mr-20 mx-6 my-6 pt-6 space-x-3 flex flex-row justify-between items-center ">
-         
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
+      <div className="right-0 top-20 absolute  w-4/6  bg-white mt-3 mr-20 mx-6 my-6 pt-6 space-x-6 flex flex-row justify-center items-center ">
+        {dailyInfo.map((daily) => (
+     <div className="bg-gradient-to-tr from-gray-100  to-gray-200 rounded-3xl flex flex-col items-center justify-center px-9 py-3 mb-16">
+      <h1 className='font-bold'>
+        {daily.day}
       </h1>
       <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
       <div className="flex space-x-1">
-        <p>30°</p>
+        <p className='font-bold opacity-90'>{daily.from}</p>
         <p>-</p>
-        <p>15°</p>
+        <p className='font-light opacity-60'>{ daily.to}</p>
       </div>
     </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
-
-    <div className="flex flex-col items-center justify-center">
-      <h1>
-        Sun
-      </h1>
-      <img className="w-20" src="https://static.vecteezy.com/system/resources/previews/010/892/336/non_2x/sun-transparent-background-free-png.png" alt="#" />
-      <div className="flex space-x-1">
-        <p>30°</p>
-        <p>-</p>
-        <p>15°</p>
-      </div>
-    </div>
+         ))}
       </div>
       </div>
   )
